ba7980e7d19b19dccddcce9494dea874a334c336
dextop <hochuel@naver.com>
2019-11-04 13:29:38 +0900

    Srv Fcm Server

---
+++ src/main/java/com/srv/util/DateUtil.java
@@ -0,0 +1,18 @@
+package com.srv.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtil {
+
+    public static String getDate(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss:SSS");
+        return dateFormat.format(new Date(System.currentTimeMillis()));
+    }
+
+
+    public static String getDate(String format){
+        SimpleDateFormat dateFormat = new SimpleDateFormat(format);
+        return dateFormat.format(new Date(System.currentTimeMillis()));
+    }
+}
